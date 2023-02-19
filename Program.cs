e5aaaf8698bfdf4746e67fef387b95e1b4eac8bf
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-02-19 20:56:10 +0300

    ödevler yapıldı

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+    
+        //girilen saya kadar olan çift sayıların toplamını yazdırır
+        Console.WriteLine("bir sayı giriniz");
+        int sayi = int.Parse(Console.ReadLine());
+
+        int toplam = 0;
+        for (int i = 1; i < sayi; i++)
+        {
+            
+            if (i % 2 == 0)
+                toplam += i;
+
+        }
+        Console.WriteLine("toplam:" + toplam);
+    }
+}
